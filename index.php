15376dd20e6e6f6dd24f5fb2d5097c478310d317
Tristan Bennett <tmbennett2011@msn.com>
2018-01-17 08:13:01 -0700

    created index and js files

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8"/>
+		<title>Snap Function Pretest</title>
+		<script src = "javaScript/script.js"></script>
+	</head>
+	<body>
+		<h1>Snap Function Pretest</h1>
+	</body>
+</html>
